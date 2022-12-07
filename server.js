800ba04d3591ef2ac45286261c7325119db55c86
ronjoshi-unc <ronjoshi@ad.unc.edu>
2022-12-07 17:26:12 -0500

    login stuff works

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 import minimist from 'minimist';
 import express from 'express';
+import bodyParser from 'body-parser';
 import path from 'path';
 import Database from 'better-sqlite3';
 import {fileURLToPath} from 'url';
+import { url } from 'inspector';
 
 const db = new Database('main.db');
 
@@ -18,12 +20,6 @@ try {
 } catch(e) {
 }
 
-const sqlFoodTable = `CREATE TABLE foodlogs ( id INTEGER PRIMARY KEY AUTOINCREMENT, username, food, ingredients, note)`;
-try {
-    db.exec(sqlFoodTable);
-} catch(e) {
-}
-
 const args = minimist(process.argv.slice(2));
 const port = args.port || 2000;
 
@@ -32,10 +28,19 @@ const thisDirectory = path.dirname(thisFile);
 // gets the directory .../a99-Group-16
 
 const app = express();
+var jsonParser = bodyParser.json();
+var urlEncodedParser = bodyParser.urlencoded({ extended: false });
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
 
 // sets the view engine. This is the most popular of the 3 most commonly used ones
 app.set('view engine', 'ejs');
 app.set('views', path.join(thisDirectory, 'views'));
+
+app.get('/', function(req, res) {
+    res.render('login');
+});
 
 app.get('/login', function(req, res) {
     res.render('login');
@@ -86,27 +91,39 @@ app.post('/register', function(req, res) {
     const username = req.body.username;
     const password = req.body.password;
     const email = req.body.email;
-    console.log(username);
 
     const rn = new Date(Date.now());
-    const sqlRegisterAttemptLog = `INSERT INTO logs (user, time, message) VALUES ('${username}', 'tried to create new user', '${rn.toISOString()}');`;
+    const sqlRegisterAttemptLog = `INSERT INTO logs (username, time, message) VALUES ('${username}', 'tried to create new user', '${rn.toISOString()}');`;
     db.exec(sqlRegisterAttemptLog)
 
-    const sqlGetUsers = db.prepare(`SELECT * FROM users WHERE user='${username}'`);
+    const sqlGetUsers = db.prepare(`SELECT * FROM users WHERE username='${username}'`);
     let row = sqlGetUsers.get();
 
     if (row === undefined) {
         const sqlInsertUser = `INSERT INTO users (username, password, email) VALUES ('${username}', '${password}', '${email}');`;
         db.exec(sqlInsertUser)
 
-        const sqlGoodRegister = `INSERT INTO logs (user, time, message) VALUES ('${username}', 'register successful', '${rn.toISOString()}');`;
+        const sqlGoodRegister = `INSERT INTO logs (username, time, message) VALUES ('${username}', 'register successful', '${rn.toISOString()}');`;
         db.exec(sqlGoodRegister)
-        res.render('settings');
+        req.app.set('user', username);
+        req.app.set('pass', password);
+        req.app.set('email', email);
+        res.redirect('/settings');
     } else {
-        const sqlBadRegister = `INSERT INTO logs (user, time, message) VALUES ('${username}', 'username taken', '${rn.toISOString()}');`;
+        const sqlBadRegister = `INSERT INTO logs (username, time, message) VALUES ('${username}', 'username taken', '${rn.toISOString()}');`;
         db.exec(sqlBadRegister)
         res.render('usernametaken');
     }
+});
+
+app.get('/settings', function(req, res){
+    const rn = new Date(Date.now());
+    let user = req.app.get('user')
+    console.log("user is: ")
+    console.log(user);
+    const sqlWentToSettings = `INSERT INTO logs (username, message, time) VALUES ('${user}', 'went to settings', '${rn.toISOString()}');`;
+    db.exec(sqlWentToSettings)
+    res.render('settings', {user: req.app.get('user'), email: req.app.get('email')});
 });
 
 app.listen(port);
